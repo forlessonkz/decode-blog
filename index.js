643faaa24cd3bb136dddf4c056c8013997a079e2
forlessonkz <forlessons.kz@gmail.com>
2024-11-12 22:38:18 +0600

    add profile page and new post page

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 3030;
+const PORT = 9097;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
@@ -17,6 +17,21 @@ app.get('/page', (req, res) => {
     res.render('postPage')
 })
 
+app.get('/signIn', (req, res) => {
+    res.render('signIn')
+})
+
+app.get('/signUp', (req, res) => {
+    res.render('signUp')
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+
+app.get('/new', (req, res) => {
+    res.render('newPost')
+})
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`)
 })
